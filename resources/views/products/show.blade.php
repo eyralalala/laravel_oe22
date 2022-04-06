2a03e3d5fcc46a63fc2a00f72f10c784f63ee7e7
eyralalala <nlsyahirah99@gmail.com>
2022-04-06 14:57:07 +0800

    product

---
+++ resources/views/products/show.blade.php
@@ -0,0 +1,46 @@
+@extends('layouts.app')
+
+
+@section('content')
+    <div class="row">
+        <div class="col-lg-12 margin-tb">
+            <div class="pull-left">
+                <h2>  </h2>
+            </div>
+            <div class="pull-right">
+                <a class="btn btn-primary" href="" title="Go back"> <i class="fas fa-backward "></i> </a>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Name:</strong>
+                <p><b>{{ $product->name }}</b></p>
+
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Description</strong>
+                <p><b>{{ $product->description }}</b></p>
+
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Price</strong>
+                <p><b>{{ $product->price }}</b></p>
+
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Date Created</strong>
+                <p><b>{{ $product->created_at }}</b></p>
+
+            </div>
+        </div>
+    </div>
+@endsection
