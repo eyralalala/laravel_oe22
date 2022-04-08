fcfdb73899e35a81f576389357ed786a2662ec40
Afrina16 <nurafrinaalimi16@gmail.com>
2022-04-08 11:05:23 +0800

    sidebar

--- resources/views/layouts/main/sidebar.blade.php
+++ resources/views/layouts/main/sidebar.blade.php
@@ -1,8 +1,4 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
-    <a href="index3.html" class="brand-link">
-        <img src="{{ asset('dist/img/oe.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-             <span class="brand-text font-weight-light">Object Expression</span>
-    </a>
     <div class="sidebar">
         <div class="user-panel mt-3 pb-3 mb-3 d-flex">
             {{-- <div class="image">
